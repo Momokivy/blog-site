ca1aa678f1c1eb3d6f48e558358f529f71e2d360
unendlicherPing <moritz31.10.18@gmail.com>
2022-02-22 15:35:43 +0100

    Changed app.ts and app.test.js I created a basic express Aplication that shows the index.html file. I commented out all the text in app.test.js because the file woldn't do anything but I need it to remember the testpaterns.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
-const lib = require('./lib/lib');
+/*-------------
+ *  app.ts
+ *-----------*/
 
-console.log(lib(4));
+//Imports
+import express from "express";
+import helmet from "helmet";
+
+// Initializes the app
+const app: express.Application = express();
+
+//Settings
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded( {extended: false} ));
+
+app.use(helmet());
+
+//Handle gets
+const handleGet = (req: express.Request, res: express.Response) => {
+  res.sendFile("./views/index.html");
+}
+
+app.get("/", handleGet);
+
+//Handle posts
+
+//Run app
+app.listen(1337);
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,7 +1,7 @@
-const lib = require("../out/lib/lib");
+// const lib = require("../out/lib/lib");
 
-describe("Lib test", () => {
-  test("adding 4 + 1 should return 5", () => {
-    expect(lib(4)).toBe(5);
-  });
-})
+// describe("Lib test", () => {
+//   test("adding 4 + 1 should return 5", () => {
+//     expect(lib(4)).toBe(5);
+//   });
+// })
